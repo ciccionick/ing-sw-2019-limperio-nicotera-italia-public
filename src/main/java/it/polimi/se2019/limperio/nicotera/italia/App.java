ccfecebcd0611341d897b6dd3c1d3eba01089edf
pietrolimperio <37225931+pietrolimperio@users.noreply.github.com>
2019-03-20 18:51:18 +0100

    Initial commit

---
+++ src/main/java/it/polimi/se2019/limperio/nicotera/italia/App.java
@@ -0,0 +1,13 @@
+package it.polimi.se2019.limperio.nicotera.italia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
