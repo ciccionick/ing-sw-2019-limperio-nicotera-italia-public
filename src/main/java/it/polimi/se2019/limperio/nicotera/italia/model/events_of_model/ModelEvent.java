680995038a0656cc48ac1ac5eeb4862ffd816d46
pietrolimperio <37225931+pietrolimperio@users.noreply.github.com>
2019-04-05 12:30:30 +0200

    Insertion of new packages

---
+++ src/main/java/it/polimi/se2019/limperio/nicotera/italia/model/events_of_model/ModelEvent.java
@@ -0,0 +1,4 @@
+package it.polimi.se2019.limperio.nicotera.italia.model.events_of_model;
+
+public class ModelEvent {
+}
---
+++ src/main/java/it/polimi/se2019/limperio/nicotera/italia/view/events_of_view/ViewEvent.java
@@ -0,0 +1,4 @@
+package it.polimi.se2019.limperio.nicotera.italia.view.events_of_view;
+
+public class ViewEvent {
+}
