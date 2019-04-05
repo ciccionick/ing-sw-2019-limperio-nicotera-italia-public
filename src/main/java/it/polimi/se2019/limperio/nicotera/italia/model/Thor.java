b802264332c663a47a80358f1d28b73309c0dba9
pietrolimperio <37225931+pietrolimperio@users.noreply.github.com>
2019-04-05 18:48:07 +0200

    Insertion of THOR class methods

--- src/main/java/it/polimi/se2019/limperio/nicotera/italia/model/Thor.java
+++ src/main/java/it/polimi/se2019/limperio/nicotera/italia/model/Thor.java
@@ -1,26 +1,68 @@
 package it.polimi.se2019.limperio.nicotera.italia.model;
 
+import it.polimi.se2019.limperio.nicotera.italia.view.events_of_view.InvolvedPlayer;
+
 import java.util.ArrayList;
 
 import static it.polimi.se2019.limperio.nicotera.italia.model.ColorOfCard_Ammo.BLUE;
+import static it.polimi.se2019.limperio.nicotera.italia.model.ColorOfCard_Ammo.RED;
 
 public class Thor extends WeaponCard {
 
 
     @Override
-    public void useWheapon(ArrayList<Integer> typeOfAttack) {
-        return;
+    public void useWeapon(ArrayList<Integer> typeOfAttack, ArrayList<InvolvedPlayer> involvedPlayers) {
+        for(int type: typeOfAttack){
+            switch (type){
+                case 1:
+                    for(InvolvedPlayer player: involvedPlayers){
+                        if(player.getEffects().contains(1))
+                            basicEffect(player.getPlayer());
+                    }
+                case 2:
+                    for (InvolvedPlayer player: involvedPlayers){
+                        if(player.getEffects().contains(2))
+                            withChainReaction(player.getPlayer());
+
+                    }
+                case 3:
+                    for(InvolvedPlayer player: involvedPlayers){
+                        if(player.getEffects().contains(3))
+                            withHighVoltage(player.getPlayer());
+                    }
+                    default:
+                        throw new IllegalArgumentException();
+            }
+        }
 
     }
 
-    public Thor() {
-        super(BLUE, "Thor", "blablabla");
-        Boolean[] kindOfAttack = {true, true, false, false};
+    Thor() {
+        super(BLUE, "Thor");
+        String description = "basic effect: Deal 2 damage to 1 target you can see.\n" +
+                "with chain reaction: Deal 1 damage to a second target that your first target can see.\n" +
+                "with high voltage: Deal 2 damage to a third target that your second target can see. You cannot use this effect unless you first use the chain reaction.\n" +
+                "Notes: This card constrains the order in which you can use its effects. (Most cards don't.) Also note that each target must be a different player.";
+        setDescription(description);
+        Boolean[] kindOfAttack = {true, true, true, false};
         setHasThisKindOfAttack(kindOfAttack);
-        setLoad(true);
-        ColorOfCard_Ammo[] buyPrice = new ColorOfCard_Ammo[]{BLUE};
+        ColorOfCard_Ammo[] buyPrice = new ColorOfCard_Ammo[]{RED};
         setPriceToBuy(buyPrice);
-        ColorOfCard_Ammo[] reloadPrice = {BLUE, BLUE};
+        ColorOfCard_Ammo[] reloadPrice = {BLUE, RED};
         setPriceToReload(reloadPrice);
     }
+
+    private void basicEffect(Player player){
+        player.assignDamage(this.getOwnerOfCard().getColorOfFigure(), 2);
+    }
+
+    private void withChainReaction(Player player){
+        player.assignDamage(this.getOwnerOfCard().getColorOfFigure(), 1);
+    }
+
+    private void withHighVoltage(Player player){
+        player.assignDamage(this.getOwnerOfCard().getColorOfFigure(), 2);
+    }
+
+
 }
--- src/main/java/it/polimi/se2019/limperio/nicotera/italia/model/TractorBeam.java
+++ src/main/java/it/polimi/se2019/limperio/nicotera/italia/model/TractorBeam.java
@@ -1,5 +1,7 @@
 package it.polimi.se2019.limperio.nicotera.italia.model;
 
+import it.polimi.se2019.limperio.nicotera.italia.view.events_of_view.InvolvedPlayer;
+
 import java.util.ArrayList;
 
 import static it.polimi.se2019.limperio.nicotera.italia.model.ColorOfCard_Ammo.BLUE;
@@ -8,19 +10,40 @@ public class TractorBeam extends WeaponCard {
 
 
     @Override
-    public void useWheapon(ArrayList<Integer> typeOfAttack) {
-        return;
+    public void useWeapon(ArrayList<Integer> typeOfAttack, ArrayList<InvolvedPlayer> involvedPlayers) {
+        switch (typeOfAttack.get(0)){
+            case 1:
+                basicMode(involvedPlayers.get(0).getPlayer(), involvedPlayers.get(0).getSquare());
+            case 4:
+                inPunisherMode(involvedPlayers.get(0).getPlayer());
+            default:
+                    throw new IllegalArgumentException();
+
+        }
 
     }
 
     public TractorBeam() {
-        super(BLUE, "TractorBeam", "blablabla");
-        Boolean[] kindOfAttack = {true, true, false, false};
+        super(BLUE, "TractorBeam");
+        String description;
+        description = "basic mode: Move a target 0, 1, or 2 squares to a square you can see, and give it 1 damage.\n" +
+                "in punisher mode: Choose a target 0, 1, or 2 moves away from you. Move the target to your square and deal 3 damage to it.\n" +
+                "Notes: You can move a target even if you can't see it. The target ends up in a place where you can see and damage it. The moves do not have to be in the same direction.";
+        setDescription(description);
+        Boolean[] kindOfAttack = {true, false, false, true};
         setHasThisKindOfAttack(kindOfAttack);
-        setLoad(true);
-        ColorOfCard_Ammo[] buyPrice = new ColorOfCard_Ammo[]{BLUE};
-        setPriceToBuy(buyPrice);
-        ColorOfCard_Ammo[] reloadPrice = {BLUE, BLUE};
+        ColorOfCard_Ammo[] reloadPrice = {BLUE};
         setPriceToReload(reloadPrice);
     }
+
+    private void basicMode(Player player, Square square){
+        player.setPositionOnTheMap(square);
+        player.assignDamage(this.getOwnerOfCard().getColorOfFigure(), 1);
+    }
+
+    private void inPunisherMode(Player player){
+        player.setPositionOnTheMap(this.getOwnerOfCard().getPositionOnTheMap());
+        player.assignDamage(this.getOwnerOfCard().getColorOfFigure(), 3);
+    }
+
 }
